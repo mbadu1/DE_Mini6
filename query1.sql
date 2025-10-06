a91cb4896226797a57afbd993420d6afb66525c0
mbadu1 <mbadukofi@gmail.com>
2025-10-06 19:54:34 -0400

    Add Query1

---
+++ query1.sql
@@ -0,0 +1,4 @@
+INSERT INTO university_rankings (institution, country, year, world_rank, score)
+VALUES ('Duke Tech', 'USA', 2014, 350, 60.5);
+
+--SELECT * from university_rankings where country = 'USA';
